28caa60710fd9418ccd6de2348ffa2a497b72463
amlmsh <martin.huelse@fh-bielefeld.de>
2021-05-05 15:33:49 +0200

    adapted everything to control a given set of servo motors (using ab and deg position values)

--- src/mex_remote_ctrl.cpp
+++ src/mex_remote_ctrl.cpp
@@ -1,31 +1,33 @@
 #include "ros/ros.h"
 #include "mex_ros_api/servo_motor_pos.h"
 #include <cstdlib>
+#include <string>
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "servo_motor_ctrl");
-  if (argc != 3)
-    {
-      ROS_INFO("usage: process two floats leftInp rightInp");
+  if (argc != 4){
+      ROS_INFO("usage: process <motorIdx int> <posUnit const char*>  <int posVal>");
       return 1;
-    }
+  };
 
   ros::NodeHandle n;
   ros::ServiceClient client = n.serviceClient<mex_ros_api::servo_motor_pos>("set_abs_servo_motor");
   mex_ros_api::servo_motor_pos srv;
-  srv.request.inpLeftSrv  = atof(argv[1]);
-  srv.request.inpRightSrv = atof(argv[2]);
-  if (client.call(srv))
-    {
-      ROS_INFO("\nLeft out: %f \nRight out: %f\n",
-	       (float)srv.response.outLeftSrv, (float) srv.response.outRightSrv);
-    }
-  else
-    {
-      ROS_ERROR("Failed to call service neural network processing.");
-      return 1;
-    }
+  srv.request.inpMotorIdxSrv  = atoi(argv[1]);
+  srv.request.inpPosUnit = std::string(argv[2]);
+  srv.request.inpPosVal = atoi(argv[3]);
+
+
+  if (client.call(srv)){
+	  ROS_INFO("\nreturned pos value: %i \nunit: %s \nof device: %i\n",
+			  (int) srv.response.outPosVal,
+			  ((std::string) srv.response.outPosUnit).c_str(),
+			  (int) srv.response.outMotorIdxSrv);
+  }else{
+	  ROS_ERROR("Failed to call service servo motor set position.");
+	  return 1;
+  }
 
   return 0;
 }
--- src/servo_motor.cpp
+++ src/servo_motor.cpp
@@ -6,54 +6,133 @@
 #include "ServoMotor.hpp"
 
 
-  ServoMotor *ptrArm;
+Pololu      *conn;
+ServoMotor  *ptrArm;
+ServoMotor* *fieldOMotors;
+int nmbMotors = 2;
+int glbArmIdx = 0; // for demo only
 
 /*
  * ROS processing functions
  *
  */
-bool setAbsPosSrvMotor(mex_ros_api::servo_motor_pos::Request   &input,
+bool setPosSrvMotor(mex_ros_api::servo_motor_pos::Request   &input,
 					   mex_ros_api::servo_motor_pos::Response  &output);
 
 
+void shutDown();
 
   
 int main(int argc, char **argv){
   ros::init(argc, argv, "servo_motor_server");
   ros::NodeHandle n;
-  ros::ServiceServer service = n.advertiseService("set_abs_servo_motor", setAbsPosSrvMotor);
+  ros::ServiceServer service = n.advertiseService("set_abs_servo_motor", setPosSrvMotor);
 
+  // init serial connection
   const char* portName = "/dev/ttyACM0";  // Linux
-  Pololu conn(portName, 9600);
-  conn.openConnection();
-  conn.getErrors();
+  conn = new Pololu(portName, 9600);
+  try{
+	  conn->openConnection();
+	  conn->getErrors();
+  }catch(ExceptionPololu *msg){
+	  std::cout << "Error Pololu: " << msg->getMsg() << endl;
+	  return 0;
+  }catch(...){
+	  std::cout << "Unknown error, while initialising Pololu serial connection.\n";
+	  return 0;
+  }
 
-  ptrArm = new ServoMotor(0, 7500, 1500	, &conn);
-  ptrArm->setMinMaxDegree(-45,45);
+  // init servomotors
+  try{
+	  fieldOMotors = new ServoMotor*[nmbMotors];
+	  for(int armIdx=0; armIdx < nmbMotors; armIdx++){
+		  ptrArm = new ServoMotor(armIdx, 6000, 4000	, conn);
+		  ptrArm->setMinMaxDegree(-90,90);
+		  fieldOMotors[armIdx] = ptrArm;
+	  }
+  }catch(ExceptionPololu *msg){
+	  std::cout << "Error Pololu: " << msg->getMsg() << endl;
+	  shutDown();
+	  return 0;
+  }catch(...){
+	  std::cout << "Unknown error, while initialising Pololu controlled servo motors.\n";
+	  shutDown();
+	  return 0;
+  }
 
-
-  ROS_INFO("Ready to process servor motor absolute positions.");
+  ROS_INFO("Ready to process servo motor absolute positions.");
   ros::spin();
 
+  shutDown();
   return 0;
 }
 
 
-bool setAbsPosSrvMotor(mex_ros_api::servo_motor_pos::Request   &input, mex_ros_api::servo_motor_pos::Response  &output){
+bool setPosSrvMotor(mex_ros_api::servo_motor_pos::Request   &input, mex_ros_api::servo_motor_pos::Response  &output){
+
+	glbArmIdx = (glbArmIdx + 1) % nmbMotors;
+	std::string unitStr;
+
+	output.outMotorIdxSrv = (int)input.inpMotorIdxSrv;
+	output.outPosVal      = (int)input.inpPosVal;
+	unitStr               = (std::string)input.inpPosUnit;
+	output.outPosUnit     = unitStr;
+
+	// check values
+	if((int)input.inpMotorIdxSrv > (nmbMotors-1)){
+		std::cout << "Motor index out of range, max. index value is " << (nmbMotors-1) << "\n";
+		return false;
+	}
+
+	// check motor index
+	if((int)input.inpMotorIdxSrv < 0){ // neg. value then sending motor active state
+		if(conn->getMovingState()){
+			output.outPosUnit = string("ACTIVE");
+			output.outPosVal = 1;
+		}else{
+			output.outPosUnit = string("INACTIVE");
+			output.outPosVal = 0;
+		}
+		return true; // finish, no position data to be set
+	}
 
 
-	output.outLeftSrv = (float)input.inpLeftSrv;
-	output.outRightSrv = (float)input.inpRightSrv;
 
+	// set position values values
 	try{
-		ptrArm->setPositionInDeg((int)input.inpLeftSrv);
+
+		ptrArm = fieldOMotors[glbArmIdx];
+		if(unitStr.compare(0,3,"ABS") == 0 ){
+			ptrArm->setPositionInAbs((int)input.inpPosVal);
+		}else if (unitStr.compare(0,3,"DEG") == 0 ){
+			ptrArm->setPositionInDeg((int)input.inpPosVal);
+		}else{
+			std::cout << "Unknown unit data: " << unitStr << " but looked for 'ABS' or 'DEG'\n";
+			return false;
+		}
 	}catch(ExceptionServoMotor *msg){
-		std::cout << msg->getMsg() << endl;
+		std::cout << msg->getMsg() << std::endl;
+		return false;
+	}catch(IException *msg){
+		std::cout << msg->getMsg() << std::endl;
+		return false;
+	}catch(...){
+		std::cout << "Unknown error while set position of robot arm.\n";
+		return false;
 	}
 
 	return true;
 }
 
 
+void shutDown(){ // free memory and close serial connection
+	  for(int armIdx=0; armIdx < nmbMotors; armIdx++){
+		  ptrArm = fieldOMotors[armIdx];
+		  delete ptrArm;
+	  }
+	  conn->closeConnection();
+	  delete conn;
 
+	  return;
+}
 
